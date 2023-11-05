e77f9117897f1f81bd6e035065c376feaf0337bb
Rahul Joshua Damanik <73378179+rahuljoshua77@users.noreply.github.com>
2023-11-05 11:53:12 +0700

    Add files via upload

---
+++ text.py
@@ -0,0 +1,13 @@
+words = '''
+#WTS Jasa Pembuatan Bot atau Automation.
+*Posting diupload oleh Bot, ingin bot seperti ini? Silahkan kontak saya
+Promosi kan bisnis anda dengan bot jadi lebih mudah dan cepatyy
+Bot Scrape Artikel
+ Bot Auto Post
+ Bot Registrasi Akun
+ Bot Web
+ Bot Isi form
+ Bot Scrape Trading
+ Bot E-commerce
+
+& dll'''
